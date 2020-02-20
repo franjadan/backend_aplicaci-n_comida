3686b87f84b2b69dbf171ab2b7d77766d7e6c1e2
raulrp3 <raul.ramirez@escuelaestech.es>
2020-02-20 23:08:19 +0100

    Acabada primer aproximacion del diseño de la pagina principal

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,27 +18,27 @@
             <div class="collapse navbar-collapse d-flex justify-content-between p-3">
               <a class="navbar-brand" href="#">Menu of the day</a>
               <div>
-                <a href="nav-link">Cerrar sesión</a>
+                <a href="nav-link" class="btn btn-outline-secondary">Cerrar sesión <i class="fas fa-sign-out-alt"></i></a>
               </div>
             </div>
         </nav>
         <div class="d-flex" id="wrapper">
-            <div class="bg-light border-right" id="sidebar-wrapper">
+            <div class="bg-light pt-4" id="sidebar-wrapper">
               <div class="list-group list-group-flush">
-                <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-check-square"></i> Pedidos</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-th-list"></i> Categorías</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-hamburger"></i> Productos</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-egg"></i> Ingredientes</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-exclamation-triangle"></i> Alérgenos</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-users"></i> Usuarios</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-comments"></i> Comentarios</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-clipboard-list"></i> Historial de pedidos</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-id-badge"></i> Perfil</a>
               </div>
             </div>
             <div id="page-content-wrapper">
-              <div class="container-fluid">
-                <h1 class="mt-4">Simple Sidebar</h1>
-                <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-                <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
-              </div>
+              <div class="container mt-5">
+                @yield('content')
             </div>
           </div>
     </div>
