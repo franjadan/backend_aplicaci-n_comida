e1f8f13cdcbb342d680f07be6d51f5fbd2fe8f13
raulrp3 <raul.ramirez@escuelaestech.es>
2020-02-22 15:10:17 +0100

    Eliminado toggle-button de navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,9 +12,6 @@
 <body>
     <div class="section">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="collapse navbar-collapse d-flex justify-content-between p-3">
               <a class="navbar-brand" href="#">Menu of the day</a>
               <div>
