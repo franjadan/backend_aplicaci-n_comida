400256c49c75bc2369af8aefdcc476544ec6cb55
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-03-04 19:08:18 +0100

    Cambiada alineación botones en editar usuario

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,24 +6,23 @@
 
     <h1>Usuario {{ $user->id }}</h1>
     
-    <form method="POST" action="{{ url("usuarios/{$user->id}") }}">
+    <form method="POST" class="d-inline" action="{{ url("usuarios/{$user->id}") }}">
                 
         {{ method_field('PUT') }}
         
         @include('users._fields')
 
         <input type="submit" class="btn btn-success" value="Guardar cambios">
-        
-        <a class="btn btn-outline-primary" href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
-
+    
     </form>
 
-    <form method="POST" class="mt-1" action="{{ url("usuarios/{$user->id}/status") }}">
+    <form method="POST" class="d-inline" action="{{ url("usuarios/{$user->id}/status") }}">
                 
         {{ method_field('POST') }}
         {{ csrf_field() }}
         
         <input type="submit" class="btn btn-warning" @if ($user->active) value="Deshabilitar usuario" @else value="Habilitar usuario" @endif>
+        <a class="btn btn-outline-primary" href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
         
     </form>
   
