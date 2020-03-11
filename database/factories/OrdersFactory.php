5420b4edf9d63a1ae8da64266bd76e8506955128
Orglash <44026373+Orglash@users.noreply.github.com>
2020-03-11 22:21:10 +0100

    Update OrdersFactory.php

--- database/factories/OrdersFactory.php
+++ database/factories/OrdersFactory.php
@@ -2,16 +2,21 @@
 
 use Faker\Generator as Faker;
 
+$faker = Faker::create('es_ES');
+
 $factory->define(App\Order::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'favourite_order_name' => $faker->word, 
         'guest_name' => $faker->name, 
         'guest_adress' => $faker->address, 
-        'guest_phone' => $faker->phone_number, 
-        'order_date' => $faker->date_time, 
-        'estimated_time' => $faker->date_time, 
-        'real_time' => $faker->date_time, 
-        'comment' => $faker->sentence
+        'guest_phone' => $faker->phoneNumber, 
+        'order_date' => $faker->dateTime, 
+        'estimated_time' => $faker->dateTime, 
+        'real_time' => $faker->dateTime, 
+        'comment' => $faker->sentence,
+        'paid' => false,
+        'state' => 'pending',
+        'user_id' => '1',
     ];
 });
