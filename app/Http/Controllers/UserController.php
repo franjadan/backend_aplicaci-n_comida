7707fe9334ca41dd03682a811c6cc1f82090e6e8
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-03-17 18:13:46 +0100

    Arreglado problema al mostrar cuando un usuario es administrador

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
                         $active = "";
 
                         if($row->isAdmin()){
-                            $tipo = "Admin";
+                            $tipo = " (Admin)";
                         }
 
                         if($row->active){
