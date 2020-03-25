0b00356b4ff18f7ef8a880ab9d66ffe726088ae7
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-03-25 13:03:39 +0100

    Creado registro y login con api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\User;
 use App\Http\Requests\CreateUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use DataTables;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -99,5 +103,69 @@ class UserController extends Controller
         $user->delete();
 
         return redirect()->route('users.index')->with('success', 'Se ha eliminado con éxito');
+    }
+
+    public function register(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [ 
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'present', 'min:6'],
+            'address' => 'required',
+            'phone' => ['required', 'regex:/(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}/'],
+        ], [
+            'first_name.required' => 'El campo nombre es obligatorio',
+            'last_name.required' => 'El campo apellido es obligatorio',
+            'email.required' => 'El campo email es obligatorio',
+            'email.email' => 'El campo email debe ser válido',
+            'email.unique' => 'El campo email debe ser único',
+            'password.required' => 'El campo contraseña debe ser obligatorio',
+            'password.min' => 'El campo contraseña debe tener mínimo 6 caracteres',
+            'address.required' => 'El campo dirección es obligatorio',
+            'phone.required' => 'El campo teléfono es obligatorio',
+            'phone.regex' => 'El teléfono debe ser válido',
+        ]);
+
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 422);         
+        } else {
+            
+            $user = new User();
+        
+            $user->forceFill([
+                'first_name' => $request->get('first_name'),
+                'last_name' => $request->get('last_name'),
+                'email' => $request->get('email'),
+                'password' => bcrypt($request->get('password')),
+                'address' => $request->get('address'),
+                'phone' => $request->get('phone'),
+                'role' => 'user',
+                'active' => true
+            ]);
+        
+            $user->save();
+
+            $token = JWTAuth::fromUser($user);
+        
+            return response()->json(compact('user','token'), 201);
+            
+        } 
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        $user = DB::table('users')->where('email', $request->get('email'))->first();
+        return response()->json(compact('user','token'), 200);
     }
 }
