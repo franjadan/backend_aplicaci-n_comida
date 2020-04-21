0954f0d47369266e194e91d083a1bc00704554ba
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-04-21 17:59:00 +0200

    Modal para cambiar el estado del usuario

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,9 +2,34 @@
 
 @section('title', "Usuario {$user->id}")
 
+@section('scripts')
+    <script src="{{ asset('js/confirm_modal.js') }}"></script>
+@endsection
+
 @section('content')
 
     <h1>Usuario {{ $user->id }}</h1>
+
+    <!--Modal deshabilitar usuario-->
+    <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmModalLabel">¡ATENCIÓN!</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ¿Estas seguro de cambiar el estado del usuario?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                <button type="button" id="acceptButton" class="btn btn-success">Aceptar</button>
+            </div>
+            </div>
+        </div>
+    </div>
     
     <form method="POST" class="d-inline" action="{{ url("usuarios/{$user->id}") }}">
                 
@@ -16,12 +41,12 @@
     
     </form>
 
-    <form method="POST" class="d-inline" action="{{ url("usuarios/{$user->id}/estado") }}">
+    <form id="deleteForm-{{ $user->id }}" method="POST" class="d-inline" action="{{ url("usuarios/{$user->id}/estado") }}">
                 
         {{ method_field('POST') }}
         {{ csrf_field() }}
         
-        <input type="submit" class="btn btn-warning" @if ($user->active) value="Deshabilitar usuario" @else value="Habilitar usuario" @endif>
+        <input data-id="{{ $user->id }}" data-toggle="modal" data-target="#confirmModal" type="button" class="btn btn-warning showModalConfirmBtn" @if ($user->active) value="Deshabilitar usuario" @else value="Habilitar usuario" @endif>
         <a class="btn btn-outline-primary" href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
         
     </form>
