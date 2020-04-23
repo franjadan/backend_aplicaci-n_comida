443d55aeb40052664dd39b7597c2e04287c914d6
raulrp3 <raul.ramirez@escuelaestech.es>
2020-04-23 18:28:10 +0200

    Mejorando estilos en la imagen de un producto o categoría al modifircar

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -3,11 +3,21 @@
 @section('title', 'Editar categoría')
 
 @section('content')
-    <img src="{{ asset($category->image) }}" alt="" id="categories_image" class="mb-3">
     <form action="{{ route('categories.edit', $category) }}" method="post" enctype="multipart/form-data">
         {{ method_field('PUT') }}
         @include('categories._fields')
-        <div class="form-group mt-5">
+        <div class="row">
+            <div class="col-8"></div>
+            <div class="col-4 mt-4 p-3 card-image">
+                <div class="card w-75 shadow">
+                    <img src="{{ asset($category->image) }}" alt="" class="card-image-top">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Imagen actual</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
             <input type="submit" class="btn btn-success" value="Modificar categoría">
             <a href="{{ route('categories') }}" class="btn btn-secondary">Volver al listado de categorías</a>
         </div>
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -3,11 +3,21 @@
 @section('title', 'Editar producto')
 
 @section('content')
-    <img src="{{ asset($product->image) }}" alt="" id="products_image" class="mb-3">
     <form action="{{ route('products.edit', $product) }}" method="post" enctype="multipart/form-data">
         {{ method_field('PUT') }}
         @include('products._fields')
-        <div class="form-group mt-5">
+        <div class="row">
+            <div class="col-8"></div>
+            <div class="col-4 mt-4 p-3 card-image">
+                <div class="card w-75 shadow">
+                    <img src="{{ asset($product->image) }}" alt="" class="card-image-top">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Imagen actual</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
             <input type="submit" class="btn btn-success" value="Modificar producto">
             <a href="{{ route('products') }}" class="btn btn-secondary">Volver al listado de productos</a>
         </div>
