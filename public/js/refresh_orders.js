b7621e0d4449b17590db0f0b67700b80d8320c9c
raulrp3 <raul.ramirez@escuelaestech.es>
2020-05-12 09:54:24 +0200

    Cogiendo url del navegador de forma dinámica para refrescar los pedidos

--- public/js/refresh_orders.js
+++ public/js/refresh_orders.js
@@ -1,9 +1,10 @@
 $(function() {
+    let url = window.location.href + 'api/pedidos';
     $('.alert-refresh-orders').hide();
     $.getJSON('/api/pedidos').done(function(data) {
         let olds = data['response']['data'];
         setInterval(function() {
-            $.getJSON('http://127.0.0.1:8000/api/pedidos').done(function(data) {
+            $.getJSON(url).done(function(data) {
                 let news = data['response']['data'];
                 if (news > olds) {
                     $('.refresh-orders-title').html('¡ATENCIÓN, tienes ' + (news - olds) + ' pedido/s nuevo/s!');
