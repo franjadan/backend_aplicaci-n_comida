2a56178cc82f7011e219ebea7e9694fa6c5f614f
Raúl Ramírez <raul.ramirez@escuelaestech.es>
2020-05-25 12:32:11 +0200

    Añadida validación unique al crear/modificar categorías y productos

--- app/Http/Requests/UpdateCategoryRequest.php
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Validation\Rule;
 use App\Category;
 
 class UpdateCategoryRequest extends FormRequest
@@ -26,7 +27,7 @@ class UpdateCategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'min:2', 'regex:/^[\pL\s\-]+$/u'],
+            'name' => ['required', 'min:2', 'regex:/^[\pL\s\-]+$/u', Rule::unique('categories', 'name')->ignore($this->category)],
             'image' => ['nullable', 'image'],
         ];
     }
@@ -37,6 +38,7 @@ class UpdateCategoryRequest extends FormRequest
             'name.required' => 'El campo nombre es obligatorio.',
             'name.min' => 'El campo nombre debe tener mínimo 2 caracteres.',
             'name.regex' => 'El campo nombre no es válido.',
+            'name.unique' => 'El campo nombre no puede coincidir con el de otra categoría.',
             'image.image' => 'El campo imagen no es válido',
         ];
     }
